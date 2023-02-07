fb9c384679378be34a0b946fa287ca0084cab951
Joshua Mazariegos Bobadilla <joshua.mazariegosbobadilla@mail.utoronto.ca>
2023-02-06 22:05:53 -0500

    setup gh-pages

--- src/index.js
+++ src/index.js
@@ -3,14 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-      <Routes>
-          <Route path='/' element={<App/>} />
-      </Routes>
       <Router>
         <App/>
       </Router>
